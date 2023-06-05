f3d8fcae08f5673738d90ca772912710dea65746
Shivansh1357 <120310888+Shivansh1357@users.noreply.github.com>
2023-06-05 13:31:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function fibonacci(num) {
-// your code here
+  if (num === 0) {
+    return 0;
+  } else if (num === 1) {
+    return 1;
+  } else {
+    return fibonacci(num - 1) + fibonacci(num - 2);
+  }
 }
+
+const n = prompt('Enter a positive integer:');
+const result = fibonacci(parseInt(n));
+
+alert('The ' + n + 'th Fibonacci number is: ' + result);
+
 
 module.exports = fibonacci;
